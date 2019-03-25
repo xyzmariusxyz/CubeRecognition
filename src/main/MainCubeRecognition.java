7773a2efc0a078c9652c295b39a40259def703b8
Marius <Marius_Kerl@web.de>
2019-03-25 07:46:10 +0100

    Update MainCubeRecognition.java

--- src/main/MainCubeRecognition.java
+++ src/main/MainCubeRecognition.java
@@ -2,9 +2,6 @@ package main;
 
 import org.opencv.core.Core;
 
-//import java.io.File;
-//import java.lang.reflect.Field;
-
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
@@ -14,8 +11,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 /**
- * The main class for a JavaFX application. It creates and handle the main
- * window with its resources (style, graphics, etc.).
+ * The main class which creates the JavaFX application and starts the prototype software for the cube recognition.
  * 
  */
 public class MainCubeRecognition extends Application
@@ -67,29 +63,9 @@ public class MainCubeRecognition extends Application
 	 * @throws IllegalArgumentException 
 	 */
 	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException
-	{		
-		/*
-		String libraryPath = "C:\\OpenCV-2.4.6\\build\\java\\x64";//"OpenCV_lib"; //C:\OpenCV-2.4.6\build\java\x64
-		System.setProperty("java.library.path", libraryPath); 
-		Field sysPath = ClassLoader.class.getDeclaredField("sys_paths");
-		sysPath.setAccessible(true); 
-		sysPath.set(null, null);
-		/*	
-		//all opencv libs must be copyed to OpenCV_lib in the project workspace
-        File folder = new File("OpenCV_lib/");
-        File[] listOfFiles = folder.listFiles(); 
-
-	    for (int i = 0; i < listOfFiles.length; i++) {
-	        if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".dll")) {
-	            File lib = new File("OpenCV_lib/" + listOfFiles[i].getName()); 
-	            System.load(lib.getAbsoluteFile().toString());
-	        }
-	    }
-	    
-	    //*/   
+	{		 
 		// load the native OpenCV library
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		//ImageRecognition.testOpenGL();
 		launch(args);
 	}
 }
